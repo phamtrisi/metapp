8c4f9e2f6ced40d72455b11a0ff2e338a44d73c4
Si Pham <spham@linkedin.com>
2015-01-13 16:18:44 -0800

    added bower

--- metapp/metapp/settings.py
+++ metapp/metapp/settings.py
@@ -3,5 +3,5 @@ DEBUG = True
 SECRET_KEY = 'metapp' # make sure to change this
 
 #'sqlite:////C:/Github/angular_flask/data/angular_flask.db'
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:@localhost/metapp'
+SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost/metapp'
 
